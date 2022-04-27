c2f2d6e2f23bf09d9eba542dd4dac7d78e42a74d
armaBurton <armanarma@armaburton.com>
2022-04-26 19:02:37 -0700

    turns out the data I was getting from the drinks site was bad, the dnd site works though

--- src/services/drinks.js
+++ src/services/drinks.js
@@ -1,9 +1,15 @@
 const fetchDrink = async () => {
   // const drink = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=brandy&api_key=1');
   
-  const drink = await fetch('https://www.dnd5eapi.co/api/monsters');
-
+  const drink = await fetch("https://www.dnd5eapi.co/api/monsters", {
+    headers: {
+      Accept: "application/json"
+    }
+  })
+  
   const drinkJson = await drink.json();
+  console.log(`|| drinkJson >`, drinkJson.results);
+  drinkJson.results.map(monster => console.log(`|| monster >`, monster));
 
   return drinkJson.drinks;
 }
